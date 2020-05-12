24428f31d7c1f4cc9e4c4a9e995358bbada269f1
avprvvp <ashev1996@mail.ru>
2020-05-12 14:30:48 +0300

    Record html to log

---
+++ app/code/Magnat/HtmlToLog/Observer/RecordHtml.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Magnat\HtmlToLog\Observer;
+
+use Psr\Log\LoggerInterface as PsrLoggerInterface;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+
+class RecordHtml implements ObserverInterface
+{
+    /**
+     * @var PsrLoggerInterface
+     */
+    private $logger;
+
+    /**
+     * MyObserver constructor.
+     *
+     * @param PsrLoggerInterface $logger
+     */
+    public function __construct(
+        PsrLoggerInterface $logger
+    )
+    {        
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Observer $observer
+     */
+    public function execute(Observer $observer)
+    {
+        $html = $observer->getEvent()->getResponse()->getBody();
+        $this->logger->notice($html);
+    }
+}
