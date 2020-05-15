3f4f06e3baca3cb6df9eb357a23d9cb08977506b
avprvvp <ashev1996@mail.ru>
2020-05-15 12:34:53 +0300

    redirect to category

--- app/code/Magnat/HelloWorld/Controller/Index/Index.php
+++ app/code/Magnat/HelloWorld/Controller/Index/Index.php
@@ -5,6 +5,7 @@ namespace Magnat\HelloWorld\Controller\Index;
 use \Magento\Framework\App\Action\Action;
 use \Magento\Framework\Controller\Result\RawFactory;
 use \Magento\Framework\App\Action\Context;
+use \Magento\Framework\Controller\Result\Redirect;
 
 
 class Index extends Action
@@ -13,20 +14,28 @@ class Index extends Action
      * @var RawFactory
      */
     protected $resultRawFactory;
+
+    /**
+     * @var Redirect
+     */
+    protected $resultRedirect;
        
     public function __construct(
         Context $context,
-        RawFactory $resultRawFactory
+        RawFactory $resultRawFactory,
+        Redirect $resultRedirect
     )
     {
         $this->resultRawFactory = $resultRawFactory;
+        $this->resultRedirect = $resultRedirect;
         return parent::__construct($context);
     }
     
     public function execute()
     {
         $result = $this->resultRawFactory->create();
-       
-        return $result->setContents('hello world');
+
+        return $this->resultRedirect->setPath('clothes.html');
+        // return $result->setContents('hello world');
     }
 }
